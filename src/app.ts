6abe4fc59b86897741899ab3720d917b902f6b52
David Minaya <davidminaya04@gmail.com>
2026-01-27 21:26:12 -0400

    Source map only in development

--- src/app.ts
+++ src/app.ts
@@ -12,8 +12,8 @@ import { connect } from './commands/connect.tsx';
 
 void yargs(hideBin(process.argv))
   .scriptName('ask')
-  .usage(`${logo}\nAI CLI to help you with commands, coding, apps and more.\n\nVersion: 1.2.1\n\nUsage: $0 <prompt..>`)
-  .version('1.2.1')
+  .usage(`${logo}\nAI CLI to help you with commands, coding, apps and more.\n\nVersion: 1.2.2\n\nUsage: $0 <prompt..>`)
+  .version('1.2.2')
   .locale('en')
   .example('$0 how to run a docker container', '')
   .example('how to setup my git account', '')
